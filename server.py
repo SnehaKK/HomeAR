61e327adc9c88abc622575cdd53405a92ada9559
SnehaKK <kulsneha04@gmail.com>
2014-12-18 21:46:03 -0800

    Adding port changes

--- server.py
+++ server.py
@@ -203,4 +203,4 @@ def contact():
 	return render_template('contactUs.html')
 
 if __name__ == '__main__':
-	app.run(host="0.0.0.0",port=5500,debug=True)
+	app.run(host="0.0.0.0",port=5000,debug=True)
